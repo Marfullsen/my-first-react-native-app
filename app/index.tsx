a79022dadc6e52a076795b721e422ac0ee0f1d9f
Francisco Marfull <marfullsen@gmail.com>
2024-12-29 00:20:03 -0300

    CHG:[Main] Change main text and use dark background color.

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View,  StyleSheet } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text style={styles.text}>Home screen</Text>
     </View>
   );
 }
@@ -11,8 +11,11 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
   },
 });
